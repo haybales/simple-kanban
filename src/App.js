0b63c9166b276663e61d2585f0e9c8080de717eb
Sam Furr <samneillfurr@gmail.com>
2017-10-04 23:23:01 -0700

    Initial commit with basic HTML for fleshing out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="header"><h1>Kanban</h1></div>
+        <form>
+          <input type="text" value="" placeholder="enter a new task..." />
+          <input type="submit" value="submit" />
+        </form>
+
+        <div className="flex-container">
+          <div className="to-do">
+            <h2>To-Do</h2>
+            <ul>
+              <li><button>&lt;</button>Task 1<button>&gt;</button></li>
+              <li><button>&lt;</button>Task 2<button>&gt;</button></li>
+            </ul>
+          </div>
+
+          <div className="working">
+            <h2>Working</h2>
+            <ul>
+              <li><button>&lt;</button>Task 1<button>&gt;</button></li>
+              <li><button>&lt;</button>Task 2<button>&gt;</button></li>
+            </ul>
+          </div>
+
+          <div className="complete">
+            <h2>Complete</h2>
+            <ul>
+              <li><button>&lt;</button>Task 1<button>&gt;</button></li>
+              <li><button>&lt;</button>Task 2<button>&gt;</button></li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
